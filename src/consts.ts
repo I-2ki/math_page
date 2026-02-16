6f09bc3aead801659a1d6a1d800ab7d331a443ae
I-2ki <62019ito@seiko.ac.jp>
2026-02-16 17:39:17 +0900

    Remove social links and update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '数学ノート';
+export const SITE_DESCRIPTION = '線形代数などの数学に関するノート';
