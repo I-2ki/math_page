f36da290faae325385ab9684ba0cad9a16defd62
I-2ki <62019ito@seiko.ac.jp>
2026-02-25 15:43:21 +0900

    refactor: とりあえずいらない機能を全て削除し、import pahtを@エイリアスに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import remarkBreaks from 'remark-breaks';
@@ -9,9 +8,9 @@ import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://i-2ki.github.io/math_page/',
+	site: 'https://i-2ki.github.io/',
 	base: '/math_page/',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx()],
 	markdown: {
 		remarkPlugins: [remarkMath, remarkBreaks],
 		rehypePlugins: [rehypeKatex],
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = '数学ノート';
 export const SITE_DESCRIPTION = '線形代数などの数学に関するノート';
